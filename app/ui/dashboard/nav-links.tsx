0473dfeee508d8b18918b13a788abb36564f0a25
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-02 15:12:35 -0400

    Capitulo 5 terminado

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import clsx from 'clsx';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -17,21 +25,31 @@ const links = [
 ];
 
 export default function NavLinks() {
+
+  const pathname = usePathname();
+
+  console.log( { pathname } );
+
   return (
     <>
-      {links.map((link) => {
+      { links.map( ( link ) => {
         const LinkIcon = link.icon;
         return (
-          <a
-            key={link.name}
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          <Link
+            key={ link.name }
+            href={ link.href }
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
+            <p className="hidden md:block">{ link.name }</p>
+          </Link>
         );
-      })}
+      } ) }
     </>
   );
 }
